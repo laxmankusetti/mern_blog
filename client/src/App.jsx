c41ce447f703f2a609e35b3a7a4a06b8ff1a727c
laxmankusetti <laxmankusetti@gmail.com>
2024-01-25 10:27:06 +0530

    initialised react router dom and created pages according to them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import DashBoard from './pages/DashBoard.jsx';
+import Projects from './pages/Projects.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-600">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<DashBoard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
