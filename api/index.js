d698376ebdfb51a1d6c38c4b9baa8d02f721509a
laxmankusetti <laxmankusetti@gmail.com>
2024-01-27 19:14:30 +0530

    created a create post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -23,6 +24,7 @@ app.listen(3000, () => {
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+app.use('/api/post', postRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
