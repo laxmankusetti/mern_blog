c547176adac6a8807f4aa195983e1672a6c885ff
laxmankusetti <laxmankusetti@gmail.com>
2024-01-27 09:47:35 +0530

    completed user image upload functionality

--- client/src/components/DashboardProfile.jsx
+++ client/src/components/DashboardProfile.jsx
@@ -1,25 +1,130 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function DashboardProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [fileUploadProgress, setFileUploadProgress] = useState(null);
+  const [fileUploadError, setFileUploadError] = useState(null);
+
+  const imageFileRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadFile();
+    }
+  }, [imageFile]);
+
+  const uploadFile = async () => {
+    setFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setFileUploadError("File must be less than 2 MBs");
+        console.log(error);
+        setFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
+
+  // service firebase.storage {
+  //   match /b/{bucket}/o {
+  //     match /{allPaths=**} {
+  //       allow read;
+  //       allow write: if
+  //       request.resource.size < 2 * 1024 * 1024 &&
+  //       request.resource.contentType.matches('image/.*')
+  //     }
+  //   }
+  // }
 
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="font-bold text-3xl text-center my-5">Profile</h1>
       <form className="flex flex-col gap-4 w-full">
-        <div className="w-32 h-32 self-center cursor-pointer">
+        <input
+          type="file"
+          onChange={handleImageChange}
+          ref={imageFileRef}
+          hidden
+        />
+        <div className="w-32 h-32 self-center cursor-pointer relative">
+          {fileUploadProgress && (
+            <CircularProgressbar
+              value={fileUploadProgress || 0}
+              text={`${fileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${fileUploadProgress / 100})`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user profile pic"
-            className="w-full h-full rounded-full object-cover border-8 border-[lightgray]"
+            className={`w-full h-full rounded-full object-cover border-8 border-[lightgray] ${
+              fileUploadProgress && fileUploadProgress < 100 && "opacity-60"
+            }`}
+            onClick={() => {
+              imageFileRef.current.click();
+            }}
+            accept="image/*"
           />
         </div>
-        <TextInput type="text" defaultValue={currentUser.username} id="username" />
+        {fileUploadError && <Alert color="failure">{fileUploadError}</Alert>}
+        <TextInput
+          type="text"
+          defaultValue={currentUser.username}
+          id="username"
+        />
         <TextInput type="email" defaultValue={currentUser.email} id="email" />
         <TextInput type="password" id="password" placeholder="Password" />
-        <Button gradientDuoTone='purpleToBlue' outline type="submit">
-            Update
+        <Button gradientDuoTone="purpleToBlue" outline type="submit">
+          Update
         </Button>
       </form>
       <div className="flex justify-between items-center text-red-600 mt-5">
--- client/src/components/DashboardSidebar.jsx
+++ client/src/components/DashboardSidebar.jsx
@@ -23,6 +23,7 @@ export default function DashboardSidebar() {
               icon={HiUser}
               label="User"
               labelColor="dark"
+              as='div'
             >
               Profile
             </Sidebar.Item>
