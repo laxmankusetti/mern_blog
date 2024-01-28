70fb4700a2d381dc4c2941eb5561eb56797c6530
laxmankusetti <laxmankusetti@gmail.com>
2024-01-28 11:29:01 +0530

    added delete functionality to the page

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { create, getPosts } from '../controllers/post.controller.js';
+import { create, deletePost, getPosts } from '../controllers/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
+router.delete('/deletepost/:postId/:userId', verifyToken, deletePost);
 router.get('/getPosts', getPosts);
 
 export default router;
--- client/src/components/DashboardPosts.jsx
+++ client/src/components/DashboardPosts.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Table } from "flowbite-react";
+import { Button, Modal, Table } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 export default function DashboardPosts() {
   const [userPosts, setUserPosts] = useState([]);
+  const [postIdToDelete, setPostIdToDelete] = useState('');
   const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
-
-  console.log(userPosts);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -41,6 +42,23 @@ export default function DashboardPosts() {
       }
       if(data.posts.length < 9){
         setShowMore(false)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleDeletePost = async () => {
+    setShowModal(false);
+    try {
+      const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`, {
+        method : 'DELETE'
+      })
+      const data = res.json();
+      if(!res.ok){
+        console.log(data.message)
+      }else{
+        setUserPosts((prev) => prev.filter((post) => post._id !== postIdToDelete))
       }
     } catch (error) {
       console.log(error)
@@ -87,7 +105,10 @@ export default function DashboardPosts() {
                 </Table.Cell>
                 <Table.Cell>{post.category}</Table.Cell>
                 <Table.Cell>
-                  <span className="text-red-500 cursor-pointer font-medium">
+                  <span className="text-red-500 cursor-pointer font-medium" onClick={() => {
+                    setShowModal(true)
+                    setPostIdToDelete(post._id)
+                  }}>
                     Delete
                   </span>
                 </Table.Cell>
@@ -107,6 +128,37 @@ export default function DashboardPosts() {
       ) : (
         <p>You have no posts yet, please add some posts to see your posts!!!</p>
       )}
+      {showModal && <Modal
+          show={showModal}
+          onClose={() => {
+            setShowModal(false);
+          }}
+          popup
+          size="md"
+        >
+          <Modal.Header />
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="h-14 w-14 mb-5 mx-auto text-gray-400 dark:text-gray-200" />
+              <h3 className="text-lg font-semibold text-gray-500 dark:text-gray-300">
+                Are you sure? You want to delete this post?
+              </h3>
+              <div className="flex justify-center items-center gap-6 my-5">
+                <Button color="failure" onClick={handleDeletePost}>
+                  Yes, I&apos;m sure
+                </Button>
+                <Button
+                  color="gray"
+                  onClick={() => {
+                    setShowModal(false);
+                  }}
+                >
+                  No, cancle
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>}
     </div>
   );
 }
